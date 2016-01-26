7706f962d096526199cc481ec6a68ed048ccba4d
Arugin <v.majackij@socialquantum.com>
2016-01-26 15:33:20 +0300

    Add coverrails

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 gem 'rake'
 gem 'yard'
+gem 'coveralls', require: false
 
 group :test do
   gem 'rspec'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,9 @@
 require 'simplecov'
 SimpleCov.start
 
+require 'coveralls'
+Coveralls.wear!
+
 require 'em_logger'
 require 'rspec'
 require 'logger'
